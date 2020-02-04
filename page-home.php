abbabe59873e16330c86bedc635d9b873183895f
Crystal Maldonado <howdycrystalm@gmail.com>
2020-02-04 10:24:07 -1000

    Added overlay and svg logo for mobile

--- page-home.php
+++ page-home.php
@@ -7,8 +7,8 @@ get_header();
 ?>
 <section>
   <div class="boxed">
+  <img class="logo" src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/logos/color-webcrumbs-main.svg"></div>
     <div id="big-video">
-    <!-- <div id="overlay"></div> -->
       <div class="mov" 
         data-src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-placeholder.png" 
         data-video="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-take-1.mp4"
