d353a90d0eb8088f6c0d4f175072bb793817da0a
Crystal Maldonado <howdycrystalm@gmail.com>
2020-02-04 12:57:10 -1000

    Added mobile styles

--- page-home.php
+++ page-home.php
@@ -5,10 +5,22 @@
  */
 get_header();
 ?>
-<section>
+<section class="homepage">
   <div class="boxed">
-  <img class="logo" src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/logos/color-webcrumbs-main.svg"></div>
-    <div id="big-video">
+    <div class="intro">
+      <img class="logo" src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/logos/color-webcrumbs-main.svg">
+      <div class="intro-headers">
+        <h1>Pave your digital footprint with WebCrumbs.</h1>  
+        <h2>Coming soon</h2>
+      </div>
+    </div>
+    <div class="icon-box">
+      <img class="icon-size" style="fill: #ebbc4e"; src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/icons/icon_instagram_yellow.svg">
+      <img class="icon-size" style="fill: #ebbc4e"; src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/icons/icon_twitter_yellow.svg">
+      <img class="icon-size" style="fill: #ebbc4e"; src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/icons/icon_facebook_yellow.svg">
+    </div>
+  </div>
+  <div id="big-video">
       <div class="mov" 
         data-src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-placeholder.png" 
         data-video="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-take-1.mp4"
