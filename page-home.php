f11d93f07ae9028fc5896a3a934868802f1a2131
Crystal Maldonado <howdycrystalm@gmail.com>
2020-02-04 08:49:03 -1000

    Added overlay to mobile and medium images

--- page-home.php
+++ page-home.php
@@ -5,12 +5,15 @@
  */
 get_header();
 ?>
-<section class="boxed">
-  <div id="big-video">
-    <div class="mov" 
-      data-src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-placeholder.png" 
-      data-video="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-take-1.mp4"
-      data-placeholder="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-placeholder.png">
+<section>
+  <div class="boxed">
+    <div id="big-video">
+    <!-- <div id="overlay"></div> -->
+      <div class="mov" 
+        data-src="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-placeholder.png" 
+        data-video="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-take-1.mp4"
+        data-placeholder="http://video.demo/wp-content/themes/wordpress-theme-starter-master/img/video/webcrumbs-placeholder.png">
+      </div>
     </div>
   </div>
 </section>
